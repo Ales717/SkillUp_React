3e199f73b9f93e5ba0cfc5cd0bc4b2fb953b808f
Ales717 <prosenjak.ales77@gmail.com>
2024-03-21 09:39:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react"
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'test',
+            day: 'mon',
+            reminder: false
+        },
+        {
+            id: 2,
+            text: 'test2',
+            day: 'mon',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'test3',
+            day: 'mon',
+            reminder: true
+        }
+    ]
+    
+)
+// Add
+const addTask = (task) =>{
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+// Delete
+const deleteTask = (id) =>{
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={()=> setShowAddTask (!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks to show'}
     </div>
-  );
+  )
 }
 
 export default App;
